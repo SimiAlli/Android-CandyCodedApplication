745d92779f70157185239367ecd5c0592d51d7fc
Simi Alli <dr.alisimt@gmail.com>
2020-10-02 00:01:52 +0100

    Phone Activity

--- app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
@@ -76,13 +76,16 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     private void createShareIntent() {
+
+        // ***
+        // TODO - Task 4 - Share the Current Candy with an Intent
+        // ***
+
         Intent shareIntent = new Intent(Intent.ACTION_SEND);
         shareIntent.setType("text/plain");
         shareIntent.putExtra(Intent.EXTRA_TEXT, SHARE_DESCRIPTION + mCandyImageUrl + HASHTAG_CANDYCODED);
         startActivity(shareIntent);
 
     }
-    // ***
-    // TODO - Task 4 - Share the Current Candy with an Intent
-    // ***
+
 }
